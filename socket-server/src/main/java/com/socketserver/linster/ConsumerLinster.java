00bff96e11aa80da4c4b9ac00879496175a92601
LvXinming <1533159440@qq.com>
2023-06-06 00:02:06 +0800

    kafka暂时不搞

--- socket-server/src/main/java/com/socketserver/linster/ConsumerLinster.java
+++ socket-server/src/main/java/com/socketserver/linster/ConsumerLinster.java
@@ -1,13 +1,13 @@
-package com.socketserver.linster;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class ConsumerLinster  {
-    public ConsumerLinster(){
-        System.out.println("启用Kafka监听器");
-        SocketConsumer socketConsumer = new SocketConsumer();
-        socketConsumer.start();
-        System.out.println("Kafka监听器启用成功");
-    }
-}
+//package com.socketserver.linster;
+//
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class ConsumerLinster  {
+//    public ConsumerLinster(){
+//        System.out.println("启用Kafka监听器");
+//        SocketConsumer socketConsumer = new SocketConsumer();
+//        socketConsumer.start();
+//        System.out.println("Kafka监听器启用成功");
+//    }
+//}
