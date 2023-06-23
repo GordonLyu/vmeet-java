14ff1008986badc9864da529c99db0bfbf64307d
LvXinming <1533159440@qq.com>
2023-06-23 18:42:09 +0800

    添加邮箱注册验证码接口

--- mail-server/src/main/java/com/mailserver/controller/MailController.java
+++ mail-server/src/main/java/com/mailserver/controller/MailController.java
@@ -2,10 +2,12 @@ package com.mailserver.controller;
 
 import com.mailserver.entity.EmailMessage;
 import com.mailserver.service.MailService;
+import com.mailserver.utils.RedisUtil;
 import com.vmeetcommon.utils.Result;
 import feign.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
@@ -23,17 +25,25 @@ public class MailController {
     @Resource
     private MailService mailService;
 
-    @Value("${spring.mail.username}")
-    private String from;
-
-    @PostMapping("/send")
-    Result sendMail(@RequestBody EmailMessage emailMessage){
-        if(emailMessage == null || "".equals(emailMessage)){
+    // 发送邮箱
+    @PostMapping("/register/send-code")
+    Result registerMailCode(@RequestBody EmailMessage emailMessage){
+        if(emailMessage == null){
             return Result.fail(400,"邮箱不能为空");
-        }else if(emailMessage.getTo().matches("^[A-Za-z0-9]+([-._][A-Za-z0-9]+)*@[A-Za-z0-9]+(-[A-Za-z0-9]+)*(\\.[A-Za-z]{2,6}|[A-Za-z]{2,4}\\.[A-Za-z]{2,3})$")){
+        }else if(!emailMessage.getTo().matches("^[A-Za-z0-9]+([-._][A-Za-z0-9]+)*@[A-Za-z0-9]+(-[A-Za-z0-9]+)*(\\.[A-Za-z]{2,6}|[A-Za-z]{2,4}\\.[A-Za-z]{2,3})$")){
             return Result.fail(400,"邮箱格式不正确");
         }
-        return mailService.sendCodeToMail(emailMessage.getTo());
+        return mailService.registerMailCode(emailMessage.getTo());
+    }
+
+    @GetMapping("/register/verify-code")
+    Result registerVerifyCode(String email, String code){
+        if(email == null || email.equals("")){
+            return Result.fail(400,"邮箱不允许为空");
+        }else if(code == null || code.equals("")){
+            return Result.fail(400,"验证码不允许为空");
+        }
+        return mailService.registerVerifyCode(email, code);
     }
 
     // 测试接口-是否正常请求
--- mail-server/src/main/java/com/mailserver/service/MailService.java
+++ mail-server/src/main/java/com/mailserver/service/MailService.java
@@ -7,9 +7,17 @@ import com.vmeetcommon.utils.Result;
  * @since 2023/6/1
  */
 public interface MailService {
+
     /**
      * 发送验证码到目标邮箱
      * @param to 目标邮箱地址
      */
-    Result sendCodeToMail(String to);
+    Result registerMailCode(String to);
+
+    /**
+     * 验证邮箱验证码
+     * @param email 邮箱
+     * @param code 用户填写的验证码
+     */
+    Result registerVerifyCode(String email, String code);
 }
