8ba158cfc838b45617ae4b4f4668cea6990ab131
fahaxikis <2727978957@qq.com>
2023-06-26 08:52:47 +0800

    :sparkles: 添加了kafka支持，后续完善其功能

--- socket-server/src/main/java/com/socketserver/SocketServerApplication.java
+++ socket-server/src/main/java/com/socketserver/SocketServerApplication.java
@@ -1,9 +1,15 @@
 package com.socketserver;
 
+import com.socketserver.linster.EsChannel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.scheduling.annotation.EnableAsync;
+
 
 @SpringBootApplication
+@EnableBinding(EsChannel.class)
 public class SocketServerApplication {
 
     public static void main(String[] args) {
